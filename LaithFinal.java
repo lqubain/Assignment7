847435777344827322b2a0f72794879ab486054a
lqubain <33189407+lqubain@users.noreply.github.com>
2017-10-28 22:39:24 -0400

    Update LaithFinal.java

--- LaithFinal.java
+++ LaithFinal.java
@@ -1,4 +1,3 @@
-package codebackup;
 import java.util.Arrays; 
 import java.util.ArrayList;
 import java.util.*; 
@@ -10,15 +9,15 @@ import java.net.*;
  * Assignment 7
  * 1068
  */
-public class CodeBackup {
+public class Asssignment7 {
     public static void main(String[] args) {
-        String script1= getWebContents("https://cis.temple.edu/~jfiore/2017/fall/1068/assignments/07/files/jaws.txt");   
-        String script = script1 + "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t " +
-                           "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t ";
+        String script1= getWebContents("http://www.gutenberg.org/cache/epub/1661/pg1661.txt"); //This method imports the script from the URL to the the string.
+        String script = addSpace(script1); 
         script = replaceVeryLC(script);
         script = replaceVeryUC(script);
         script = replaceVeryMC(script);
-        System.out.println(replace(script));
+        String finalScript = new String(replace(script));
+        saveDoc(finalScript, "brody-crib-sheet.txt");
     }
     public static String replaceVeryLC(String script) {
         String newScript = script.replace(" very", " wicked");
@@ -63,7 +62,7 @@ public class CodeBackup {
             ArrayList<Character> ahARR = new ArrayList<Character>();
             ahARR.add('a');
             ahARR.add('h');
-            if (((scriptARR.get(count-1) == 'e' && scriptARR.get(count-2) == 'e')) && (scriptARR.get(count+1) == ' ' || isSymbol(scriptARR.get(count+1)))){
+            if (((scriptARR.get(count-1) == 'e' && scriptARR.get(count-2) == 'e')) && (scriptARR.get(count+1) == ' ' || isSymbol(scriptARR.get(count+1)))&& (scriptARR.get(count) == 'r')){
                   scriptARR.set(count, 'y');
                   scriptARR.addAll(count+1, ahARR);
               }
@@ -73,7 +72,7 @@ public class CodeBackup {
             ArrayList<Character> ahARR = new ArrayList<Character>();
             ahARR.add('A');
             ahARR.add('H');
-            if (((scriptARR.get(count-1) == 'E' && scriptARR.get(count-2) == 'E')) && (scriptARR.get(count+1) == ' ' || isSymbol(scriptARR.get(count+1)))){
+            if ((((scriptARR.get(count-1) == 'E' && scriptARR.get(count-2) == 'E')) && (scriptARR.get(count+1) == ' ' || isSymbol(scriptARR.get(count+1))))&& (scriptARR.get(count) == 'R')){
                   scriptARR.set(count, 'Y');
                   scriptARR.addAll(count+1, ahARR);
               }
@@ -136,34 +135,92 @@ public class CodeBackup {
         return false;
     }
     public static String getWebContents(String url) {
-    BufferedReader br = null;
-    String result = null;
-    try {
-	URL toFetch = new URL(url);
-	br = new BufferedReader(new InputStreamReader(toFetch.openStream()));
-	StringBuilder ret = new StringBuilder();
-	String line = br.readLine();
-	while (line != null) {
-	    ret.append(line+'\n');
-	    line = br.readLine();
-	}
-	result = ret.toString();
-    } catch (MalformedURLException ex) {
-	ex.printStackTrace();
-	System.err.println("Malformed URL:  " + url);
-    } catch (IOException ioe) {
-	ioe.printStackTrace();
-	System.err.println("Error reading from URL:  " + url);
-    } finally {
-	if(br!=null) {
-	    try {
-		br.close();
-	    } catch (IOException ex) {
-		ex.printStackTrace();
-		System.err.println("Couldn't close connection to url properly:  " + url);
-	    }
-	}
-	return result;
+        BufferedReader br = null;
+        String result = null;
+        try {
+            URL toFetch = new URL(url);
+            br = new BufferedReader(new InputStreamReader(toFetch.openStream()));
+            StringBuilder ret = new StringBuilder();
+            String line = br.readLine();
+            while (line != null) {
+                ret.append(line+'\n');
+                line = br.readLine();
+            }
+            result = ret.toString();
+        } catch (MalformedURLException ex) {
+            ex.printStackTrace();
+            System.err.println("Malformed URL:  " + url);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            System.err.println("Error reading from URL:  " + url);
+        } finally {
+            if(br!=null) {
+                try {
+                    br.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    System.err.println("Couldn't close connection to url properly:  " + url);
+                }
+        }
+        return result;
+        }
     }
+    public static void saveDoc(String contents, String filename) {
+        PrintWriter pw = null;
+        try {
+            pw = new PrintWriter(new FileWriter(new File(filename)));
+            pw.print(contents);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            System.err.println("Error writing to file:  " + filename);
+        } finally {
+            if(pw!=null) {
+                pw.close();
+            }
+        }
+    }
+    public static String addSpace (String script1){
+        String script = script1 + "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t "+
+        "\t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t";
+        return script; 
     }
 }
